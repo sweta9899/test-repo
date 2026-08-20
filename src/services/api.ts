b950db91082858e6da7cca5b8406c48a25c0510f
sweta <sweta28chaurasiya@gmail.com>
2026-08-20 17:41:11 +0530

    change end point

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ??
-    "https://admin-moderator-backend-staging.up.railway.app",
+    "https://admin-moderator-backend-staging.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
